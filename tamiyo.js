d3a89758a1f2cab5d84aaf877285c2f7df1f2d6a
Ash K <48103017+Alcadies@users.noreply.github.com>
2021-06-12 15:59:17 -0400

    Or equal to

--- tamiyo.js
+++ tamiyo.js
@@ -1066,7 +1066,7 @@ async function goToBed(message) {
         var theTimes = await bot.channels.get("531433553225842700").fetchMessage("853349767119896618");
         var hours = await theTimes.content.split("\n");
         var d = new Date().getHours();
-        if (d > hours[1] && d - hours[1] < hours[2]) {
+        if (d >= hours[1] && d - hours[1] < hours[2]) {
             message.channel.send("No tale should be discarded.");
             message.author.send("Get some sleep! Being awake is fun, but you'll have a better day tomorrow if you're rested.");
         }
